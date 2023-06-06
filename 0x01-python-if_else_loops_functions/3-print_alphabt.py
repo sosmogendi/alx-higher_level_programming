b2d5205183af7a929a4bd702a7bac2fefe282117
sospeter <sosmogendi@gmail.com>
2023-06-06 13:28:33 -0700

    Some ASCII alphabets

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
 print("{}".format("".join(chr(i) for i in range(97, 123)
-        if chr(i) not in ['q', 'e'])), end="")
+      if chr(i) not in ['q', 'e'])), end="")
