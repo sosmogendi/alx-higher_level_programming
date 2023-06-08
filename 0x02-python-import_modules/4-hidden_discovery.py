2aed11608080e31111db633de335112400d765b7
sospeter <sosmogendi@gmail.com>
2023-06-08 14:22:38 -0700

    prints all the names defined by the compiled module hidden_4.pyc

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import dis
+import importlib.util
+
+if __name__ == "__main__":
+    spec = importlib.util.spec_from_file_location("hidden_module", "hidden_4.pyc")
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+
+    hidden_names = []
+    for name in dir(module):
+        if not name.startswith("__"):
+            hidden_names.append(name)
+
+    hidden_names.sort()
+
+    for name2 in hidden_names:
+        print(name2)
