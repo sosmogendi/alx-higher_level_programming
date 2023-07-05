ef72aaef8ea0eb28cd03d58e8ee4dc561909bd0b
sospeter <sosmogendi@gmail.com>
2023-07-05 13:05:05 -0700

    Low memory cost

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,11 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+
 class LockedClass:
+    """
+    Class that prevents the dynamic creation of new instance attributes, except for 'first_name'.
+    """
+
     def __setattr__(self, name, value):
-        if name != 'first_name':
+        if not hasattr(self, name) and name != 'first_name':
             raise AttributeError("'LockedClass' object has no attribute '{}'".format(name))
+        super().__setattr__(name, value)
