576775a13f8582bdccb282df249f10e926920caa
sospeter <sosmogendi@gmail.com>
2023-07-10 14:13:53 -0700

    Geometry module

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+5-base_geometry module
+
+This module contains the definition of the BaseGeometry class.
+"""
+
+class BaseGeometry:
+    """
+    BaseGeometry class
+    """
+    pass
