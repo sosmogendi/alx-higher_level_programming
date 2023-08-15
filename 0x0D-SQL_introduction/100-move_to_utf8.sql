a1792a71fc9186e318d2859b8882b42213f34fdd
Sospeter <sosmogendi@gmail.com>
2023-08-15 13:12:51 -0700

    Convert database to UTF8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,10 +1,6 @@
 -- Convert the hbtn_0c_0 database to UTF8
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
+USE hbtn_0c_0;
 -- Convert the first_table table to UTF8
-ALTER TABLE hbtn_0c_0.first_table
-CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
+ALTER TABLE first_table
 -- Convert the name field in the first_table to UTF8
-ALTER TABLE hbtn_0c_0.first_table
-MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
