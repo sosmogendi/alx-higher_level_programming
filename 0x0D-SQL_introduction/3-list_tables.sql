5c72f2a1c01075f517fd697b04ec9dead1ec4e2d
Sospeter <sosmogendi@gmail.com>
2023-08-15 07:44:11 -0700

    List tables

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,2 +1,5 @@
--- List all the tables of mysql database
-SHOW TABLES FROM mysql;
+-- Change the active database to 'mysql'
+USE mysql;
+
+-- Display a list of all tables in the current database
+SHOW TABLES;
