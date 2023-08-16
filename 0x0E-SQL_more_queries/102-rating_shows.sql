3afb4782c5983a5208f9859f02db889855e32367
Sospeter <sosmogendi@gmail.com>
2023-08-16 15:17:28 -0700

    updates

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,6 @@
 -- List all shows from hbtn_0d_tvshows_rate by their rating
-SELECT ts.title, SUM(tsr.rate) AS rating_sum
+SELECT ts.title, SUM(tsr.rate) AS rating
 FROM tv_shows ts
 LEFT JOIN tv_show_ratings tsr ON ts.id = tsr.show_id
 GROUP BY ts.title
-ORDER BY rating_sum DESC;
+ORDER BY rating DESC;
