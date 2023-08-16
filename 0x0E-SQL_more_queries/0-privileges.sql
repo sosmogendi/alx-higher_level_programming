b1636b8544147d51448c232e72190eeffdea0709
Sospeter <sosmogendi@gmail.com>
2023-08-16 06:34:48 -0700

    Create MySQL user user_0d_1 with all privileges

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,4 @@
--- Show privileges for user user_0d_1 on localhost
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- Show privileges for users user_0d_1 and user_0d_2 on localhost
 
--- Show privileges for user user_0d_2 on localhost
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
