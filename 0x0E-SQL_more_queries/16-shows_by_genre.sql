66cc3bed245fe9f5867ea572ca69cd2c3088f82f
Sospeter <sosmogendi@gmail.com>
2023-08-16 14:32:16 -0700

    Shows and linked genres from hbtn_0d_tvshows

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- List all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
+SELECT ts.title, IFNULL(tg.name, 'NULL') AS name
+FROM tv_shows ts
+LEFT JOIN tv_show_genres tsg ON ts.id = tsg.show_id
+LEFT JOIN tv_genres tg ON tsg.genre_id = tg.id
+ORDER BY ts.title ASC, name ASC;
