642a583f77b5b4b72748d249a4f6ef2062fbffb4
Sospeter <sosmogendi@gmail.com>
2023-08-16 14:36:20 -0700

    Create MySQL user user_0d_1 with privileges

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,8 +1,3 @@
 -- Create user_0d_1 if it doesn't already exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
--- Grant all privileges to user_0d_1
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-
--- Flush privileges to apply changes
-FLUSH PRIVILEGES;
