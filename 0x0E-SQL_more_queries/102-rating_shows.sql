e21d87174487c1faa496454dafadd722605e3d84
Sospeter <sosmogendi@gmail.com>
2023-08-16 15:05:34 -0700

    List shows by their rating sum

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,5 +1,5 @@
 -- List all shows from hbtn_0d_tvshows_rate by their rating
-SELECT ts.title, COALESCE(SUM(tsr.rate), 0) AS rating_sum
+SELECT ts.title, IFNULL(SUM(tsr.rate), 0) AS rating_sum
 FROM tv_shows ts
 LEFT JOIN tv_show_ratings tsr ON ts.id = tsr.show_id
 GROUP BY ts.title
