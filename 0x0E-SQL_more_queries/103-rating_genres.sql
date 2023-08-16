b0079d1fff12fa125511669d40760a202ebaaeda
Sospeter <sosmogendi@gmail.com>
2023-08-16 15:19:00 -0700

    List genres by rating sum in hbtn_0d_tvshows_rate

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,7 +1,7 @@
 -- List all genres in the database hbtn_0d_tvshows_rate by their rating
-SELECT tg.name, SUM(tsr.rate) AS rating_sum
+SELECT tg.name, SUM(tsr.rate) AS rating
 FROM tv_genres tg
 LEFT JOIN tv_show_genres tsg ON tg.id = tsg.genre_id
 LEFT JOIN tv_show_ratings tsr ON tsg.show_id = tsr.show_id
 GROUP BY tg.name
-ORDER BY rating_sum DESC;
+ORDER BY rating DESC;
