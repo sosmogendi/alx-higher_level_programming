943ad1d3221156bb43c5b6ffae7f12ae75e9633f
Sospeter <sosmogendi@gmail.com>
2023-08-16 14:11:04 -0700

    All genres associated with the TV show "Dexter" from the hbtn_0d_tvshows database

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,7 @@
+-- Use the hbtn_0d_tvshows database to list all genres of the show Dexter
+SELECT tg.name
+FROM tv_genres tg
+JOIN tv_show_genres tsg ON tg.id = tsg.genre_id
+JOIN tv_shows ts ON tsg.show_id = ts.id
+WHERE ts.title = 'Dexter'
+ORDER BY tg.name ASC;
