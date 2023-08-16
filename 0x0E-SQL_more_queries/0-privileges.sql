4a045123172802a2516fa5bdf451eae11363e80f
Sospeter <sosmogendi@gmail.com>
2023-08-16 06:00:41 -0700

    List all privileges of the MySQL users user_0d_1 and user_0d_2

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- Show privileges for user user_0d_1 on localhost
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Show privileges for user user_0d_2 on localhost
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
