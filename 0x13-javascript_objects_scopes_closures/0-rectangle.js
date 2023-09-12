532f63e6be84a235ae7e328ebac4c97a2b60685c
sospeter <sosmogendi@gmail.com>
2023-09-12 10:23:10 -0700

    Empty Rectangle

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,5 +1,7 @@
 class Rectangle {
-  /* The class is currently empty, with no properties or methods. */
+	constructor() {
+		/* The class is currently empty, with no properties or methods. */
+	}
 }
 
 module.exports = Rectangle;
