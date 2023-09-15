fddc167b2bcfb698dec81159468440abf8565900
Sospeter <sosmogendi@gmail.com>
2023-09-15 15:23:59 -0700

    Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,36 +3,40 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    # Check if three arguments are provided
     if len(sys.argv) != 4:
         print("Usage: {} <mysql_username> <mysql_password> <database_name>".format(sys.argv[0]))
         sys.exit(1)
 
-    # Retrieve command line arguments
     mysql_username = sys.argv[1]
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
 
-    # Connect to the MySQL server
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=mysql_username,
-        passwd=mysql_password,
-        db=database_name
-    )
+    try:
+        # Connect to the MySQL server
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=mysql_username,
+            passwd=mysql_password,
+            db=database_name
+        )
 
-    # Create a cursor object
-    cursor = db.cursor()
+        # Create a cursor object
+        cursor = db.cursor()
 
-    # Execute the SQL query to select all states and order by ID
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+        # Execute the SQL query to select all states and order by ID
+        cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-    # Fetch all rows and print them
-    states = cursor.fetchall()
-    for state in states:
-        print(state)
+        # Fetch all rows and print them
+        states = cursor.fetchall()
+        for state in states:
+            print(state)
 
-    # Close the cursor and database connection
-    cursor.close()
-    db.close()
+    except MySQLdb.Error as e:
+        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
+        sys.exit(1)
+
+    finally:
+        # Close the cursor and database connection
+        cursor.close()
+        db.close()
