69d908b086bffe7129c12c481595c4dcd1ea2130
Sospeter <sosmogendi@gmail.com>
2023-09-16 10:08:35 -0700

    Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,44 +1,43 @@
 #!/usr/bin/python3
-import MySQLdb
 import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    # Check if all three arguments (username, password, database name) are provided
     if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database_name>".format(sys.argv[0]))
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
         sys.exit(1)
 
-    # Get arguments
     username = sys.argv[1]
     password = sys.argv[2]
-    database_name = sys.argv[3]
+    database = sys.argv[3]
 
+    # Connect to the MySQL server
     try:
-        # Connect to the MySQL server
-        db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=username,
-            passwd=password,
-            db=database_name,
-            charset="utf8"
-        )
-
-        # Create a cursor object to execute SQL queries
-        cursor = db.cursor()
-
-        # Execute the SELECT query
-        cursor.execute("SELECT * FROM states ORDER BY id ASC")
-
-        # Fetch and print the results
-        results = cursor.fetchall()
-        for row in results:
-            print(row)
-
-        # Close the cursor and the database connection
-        cursor.close()
-        db.close()
-
+        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
     except MySQLdb.Error as e:
-        print("MySQL Error: {}".format(e))
+        print("Error connecting to the database:", e)
         sys.exit(1)
+
+    # Create a cursor object to interact with the database
+    cursor = db.cursor()
+
+    # Execute the SELECT query
+    try:
+        cursor.execute("SELECT * FROM states ORDER BY id ASC")
+        states = cursor.fetchall()
+    except MySQLdb.Error as e:
+        print("Error executing query:", e)
+        sys.exit(1)
+
+    # Use a set to keep track of unique states
+    unique_states = set()
+
+    # Display the results without duplicates
+    for state in states:
+        if state[1] not in unique_states:
+            print(state)
+            unique_states.add(state[1])
+
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
