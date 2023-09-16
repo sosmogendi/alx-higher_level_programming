593ce0b9752d0e572e5224efb164033ee68f02d0
Sospeter <sosmogendi@gmail.com>
2023-09-16 09:40:06 -0700

    Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-import sys
 import MySQLdb
+import sys
 
 if __name__ == "__main__":
+    # Check if all three arguments (username, password, database name) are provided
     if len(sys.argv) != 4:
-        print("Usage: {} <mysql_username> <mysql_password> <database_name>".format(sys.argv[0]))
+        print("Usage: {} <username> <password> <database_name>".format(sys.argv[0]))
         sys.exit(1)
 
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
+    # Get arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
     database_name = sys.argv[3]
 
     try:
@@ -16,27 +18,27 @@ if __name__ == "__main__":
         db = MySQLdb.connect(
             host="localhost",
             port=3306,
-            user=mysql_username,
-            passwd=mysql_password,
-            db=database_name
+            user=username,
+            passwd=password,
+            db=database_name,
+            charset="utf8"
         )
 
-        # Create a cursor object
+        # Create a cursor object to execute SQL queries
         cursor = db.cursor()
 
-        # Execute the SQL query to select all states and order by ID
+        # Execute the SELECT query
         cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-        # Fetch all rows and print them
-        states = cursor.fetchall()
-        for state in states:
-            print(state)
+        # Fetch and print the results
+        results = cursor.fetchall()
+        for row in results:
+            print(row)
 
-    except MySQLdb.Error as e:
-        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
-        sys.exit(1)
-
-    finally:
-        # Close the cursor and database connection
+        # Close the cursor and the database connection
         cursor.close()
         db.close()
+
+    except MySQLdb.Error as e:
+        print("MySQL Error: {}".format(e))
+        sys.exit(1)
