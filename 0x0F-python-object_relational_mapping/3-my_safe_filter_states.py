c2f3bb3761a853910c005109f087ef9811d08e93
Sospeter <sosmogendi@gmail.com>
2023-09-16 12:31:45 -0700

    SQL Injection...

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
             passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
     match = sys.argv[4]
-    cur.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY %s", (match, ))
     results = cur.fetchall()
     for row in results:
         print(row)
