d7fe23db5f8f3bd4877de8b72c95af9701a69cc1
root <sosmogendi@gmail.com>
2023-09-28 08:19:56 -0700

    cURL only methods

---
+++ 0x10-python-network_0/3-methods.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# This script takes a URL as an argument and displays the allowed HTTP methods
+
+# Check if a URL argument is provided
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+# Extract the URL from the command-line argument
+url="$1"
+
+# Use curl to send an HTTP OPTIONS request to the URL and display the allowed methods
+curl -sI "$url" | grep "Allow" | cut -d ' ' -f 2-
