8a8a167eb3a5c4106188fadb4f5123267531ac27
root <sosmogendi@gmail.com>
2023-09-28 08:32:33 -0700

    Send a GET request

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
-# This script takes a URL as an argument, sends a GET request using curl, and displays the body of the response for a 200 status code.
+# Send a GET request to a URL and display the response body for a 200 status code
 
-# Check if the URL argument is provided
-if [ -z "$1" ]; then
+# Check if a URL argument is provided
+if [ $# -ne 1 ]; then
     echo "Usage: $0 <URL>"
     exit 1
 fi
 
-# Send a GET request to the provided URL and follow redirects (-L), then display the response body
-curl -sL "$1"
+# Extract the URL from the command-line argument
+url="$1"
+
+# Send a HEAD request to get the response code
+response_code=$(curl -L -s -X HEAD -w "%{http_code}" "$url")
+
+# Check if the response code is 200 (OK)
+if [ "$response_code" == '200' ]; then
+    # If the response code is 200, send a GET request and display the response body
+    curl -L -s "$url"
+fi
