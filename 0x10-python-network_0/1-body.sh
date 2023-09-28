ed00cbe096ea7a414965179d69390fbd6e63651d
root <sosmogendi@gmail.com>
2023-09-28 08:44:15 -0700

    GET request

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 # sends a GET request to the URL
 # Check if a URL argument is provided
-
 target_url="$1"
 
-# Check if the HTTP status code from the HEAD request is 200
 if [ $(curl -L -s -X HEAD -w "%{http_code}" "$target_url") == '200' ]; then
-    # If the status code is 200, fetch and display the response body
     curl -Ls "$target_url"
 fi
