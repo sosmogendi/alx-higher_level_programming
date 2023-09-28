816c99ffdd0975266aec2ccec45464496c794c84
root <sosmogendi@gmail.com>
2023-09-28 09:40:43 -0700

    Request to 0.0.0.0:5000/catch_me

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-
-# Send a POST request to the specified URL with the required headers
-curl -s -L -X PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
+# Bash script makes a request to 0.0.0.0:5000/catch_me and displays the response
+# Send the request and save the response to a temporary file
+curl -s -L -X PUT -d "user_id=98" -H "Origin: HolbertonSchool" -o /tmp/response 0.0.0.0:5000/catch_me
+# Extract and display the message from the response file
+cat /tmp/response
