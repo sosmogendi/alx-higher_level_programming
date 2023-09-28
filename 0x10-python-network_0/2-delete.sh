1c036bc7ee1ca6686f044ad8ccd36e81c1f7b26d
root <sosmogendi@gmail.com>
2023-09-28 08:29:19 -0700

    DELETE request

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-# This script sends a DELETE request to the URL passed as the first argument and displays the body of the response.
-
-# Check if the URL argument is provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Send a DELETE request to the provided URL and display the response body
+# A script to send delete request to the url passed
 curl -s -X DELETE "$1"
