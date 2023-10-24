b4ea1d3393981d009bff678c1a86203f72771731
Sos <sosmogendi@gmail.com>
2023-10-24 12:11:50 -0700

    script that gets the contents of a webpage and stores it in a file

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+if (!url || !filePath) {
+  console.error('Usage: node 5-request_store.js <URL> <file_path>');
+  process.exit(1);
+}
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else if (response.statusCode === 200) {
+    fs.writeFile(filePath, body, 'utf-8', (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log(`Content saved to ${filePath}`);
+      }
+    });
+  } else {
+    console.error(`Status code: ${response.statusCode}`);
+  }
+});
