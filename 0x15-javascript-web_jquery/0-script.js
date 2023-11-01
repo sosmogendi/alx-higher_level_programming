f16a91c7be47d51642056462b6ec03cdbe765048
Sos <sosmogendi@gmail.com>
2023-11-01 08:48:44 -0700

    No JQuery

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+  // Select the <header> element
+  const headerElement = document.querySelector("header");
+
+  // Check if the element was found
+  if (headerElement) {
+    headerElement.style.color = "#FF0000";
+  }
+});
